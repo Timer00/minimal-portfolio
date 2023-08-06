9b579a05f0f1aab784d6f53e5bb7b1153dcf0337
Théo Carrara <theocarraraleao@gmail.com>
2023-08-06 13:51:44 +0200

    Move LightDarkToggle to it's own component

---
+++ src/components/LightDarkToggle.tsx
@@ -0,0 +1,18 @@
+import { twMerge } from 'tailwind-merge';
+
+interface ButtonModeSwitchProps {
+  className?: string;
+  switchMode: () => void;
+}
+
+export const LightDarkToggle = ({ className, switchMode }: ButtonModeSwitchProps) => {
+
+  return (
+    <button onClick={switchMode} className={twMerge('font-bold', className)}>
+      <div className="ease-in duration-1000 relative inline-block">
+        <div className={`ease-in duration-1000 absolute w-[10vw] sm:w-[5vw] h-[10vw] sm:h-[5vw] border-2 border-primary rounded-full`}></div>
+        <div className={`ease-in duration-1000 absolute w-[10vw] sm:w-[5vw] h-[10vw] sm:h-[5vw] bg-primary rounded-full left-[4vw] sm:left-[2vw] top-[4vw] sm:top-[2vw]`}></div>
+      </div>
+    </button>
+  );
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,9 +5,10 @@ import useReverseScroll from "~/hooks/reverseScroll";
 import { Hero } from "~/sections/Hero";
 import { Tech } from "~/sections/Tech";
 import { Projects } from "~/sections/Projects";
+import { LightDarkToggle } from "~/components/LightDarkToggle";
 
 export default function Home() {
-  const [isDark, flip] = useDarkMode();
+  const [, flip] = useDarkMode();
   useReverseScroll('reverse-scroll')
   const dimension = useResponsiveSquare(0.9);
 
@@ -19,12 +20,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="transition-all ease-in duration-1000 min-h-screen bg-secondary flex justify-center items-center text-primary">
-        <button onClick={flip} className='font-bold absolute top-[6vh] sm:top-[3vh] right-[20vw] sm:right-[10vw]'>
-          <div className="ease-in duration-1000 relative inline-block">
-            <div className={`ease-in duration-1000 absolute w-[10vw] sm:w-[5vw] h-[10vw] sm:h-[5vw] border-2 border-primary rounded-full`}></div>
-            <div className={`ease-in duration-1000 absolute w-[10vw] sm:w-[5vw] h-[10vw] sm:h-[5vw] bg-primary rounded-full left-[2vw] top-[2vw]`}></div>
-          </div>
-        </button>
+        <LightDarkToggle switchMode={flip} className='absolute top-[6vh] sm:top-[3vh] right-[20vw] sm:right-[10vw]'/>
         <div className="container grid grid-cols-5 grid-rows-2" style={{ width: dimension, height: dimension }}>
           <Hero className='grid-rows-3 col-span-3 row-span-1'/>
           <Tech className='grid-cols-3 col-span-2 row-span-2'/>
